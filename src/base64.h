10a822909fd67e5e5b10245b169285699414973d
Nathan Hui <ntlhui@gmail.com>
2022-08-26 12:57:56 -0700

    Added extern

--- src/base64.h
+++ src/base64.h
@@ -11,6 +11,10 @@
 #include <stdint.h>
 #include <stddef.h>
 
+#ifdef __cplusplus
+extern "C"
+{
+#endif
 int b64_encode(const uint8_t *in, size_t ilen, char *out, size_t *olen);
 
 int b64_decode(const char *in, size_t ilen, uint8_t *out, size_t *olen);
@@ -18,5 +22,8 @@ int b64_decode(const char *in, size_t ilen, uint8_t *out, size_t *olen);
 int urlsafe_b64_encode(const uint8_t *in, size_t ilen, char *out, size_t *olen);
 
 int urlsafe_b64_decode(const char *in, size_t ilen, uint8_t *out, size_t *olen);
+#ifdef __cplusplus
+}
+#endif
 
 #endif // B64_H_
